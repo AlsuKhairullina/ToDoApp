770f9995d0aa0fa2f781f8e508261d97ce7a9e01
Alsu Khairullina <alcukhairullina@yandex.ru>
2024-08-25 15:03:25 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Алсу Хайруллина on 25.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
